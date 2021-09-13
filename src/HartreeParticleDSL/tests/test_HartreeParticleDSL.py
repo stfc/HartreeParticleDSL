67e688492e31ee2b2f855766c36f2fba272c13dd
Aidan Chalk <3043914+LonelyCat124@users.noreply.github.com>
2021-09-13 13:09:06 +0100

    Adding an extra test

--- src/HartreeParticleDSL/tests/test_HartreeParticleDSL.py
+++ src/HartreeParticleDSL/tests/test_HartreeParticleDSL.py
@@ -7,3 +7,7 @@ def test_single_instance():
     with pytest.raises(SingletonInstanceError) as excinfo:
         b = HartreeParticleDSL._HartreeParticleDSL()
     assert "Only one instance of _HartreeParticleDSL is allowed" in str(excinfo.value)
+
+def test_config_init():
+    a = HartreeParticleDSL.Config()
+    assert a.config_type['space'] == {'type' : 'struct space_type', 'is_array' : False }
