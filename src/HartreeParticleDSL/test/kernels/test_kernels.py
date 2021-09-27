6b2310838a4fcf8a82c2753aaf945e32192aff5f
Aidan Chalk <3043914+LonelyCat124@users.noreply.github.com>
2021-09-27 16:45:13 +0100

    Tests to cover the kernel code that wasn't tested (#11)
    * Tests to cover the kernel code that wasn't tested
    
    * Fixed two missing __ in the init for the kernel base class

---
+++ src/HartreeParticleDSL/test/kernels/test_kernels.py
@@ -0,0 +1,18 @@
+import ast
+import inspect
+from HartreeParticleDSL.kernel_types.kernels import *
+
+def test_kernel():
+    kern = kernel()
+    assert kern.get_kernel_tree() is None
+
+def test_main_function_wrapper():
+    a = ast.parse("a = a + 1")
+    mfw = main_function_wrapper(a)
+    assert mfw.get_kernel_tree() is a
+
+def main():
+    a = a + 1
+def test_main_declaration_function():
+    parser = main_declaration(main)
+    assert isinstance(parser, main_function_wrapper)
