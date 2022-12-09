a1ef6c9e980ecba7ee556f7b896ba69e1a2e9c3d
Aidan Chalk <3043914+LonelyCat124@users.noreply.github.com>
2022-12-09 10:46:29 +0000

    Missing suffix again?

--- setup.py
+++ setup.py
@@ -78,13 +78,13 @@ if __name__ == '__main__':
 
     EGS_DIR = os.path.join(BASE_PATH, "examples")
     INSTALL_PATH = os.path.join("share", "HartreeParticleDSL", "examples")
-    VALID_SUFFIXES = ["90", "py", "md", ".c", ".cl", "Makefile", ".mk", ".cpp", ".hpp"]
+    VALID_SUFFIXES = ["90", "py", "md", ".c", ".cl", "Makefile", ".mk", "cpp", "hpp"]
     EXAMPLES = get_files(EGS_DIR, INSTALL_PATH, VALID_SUFFIXES)
 
     LIBS_DIR = os.path.join(BASE_PATH, "lib")
     INSTALL_PATH = os.path.join("share", "HartreeParticleDSL", "lib")
     VALID_SUFFIXES = ["90", "sh", "py", "md", "Makefile", ".mk",
-                      ".jinja", "doxyfile"]
+                      ".jinja", "doxyfile", "cpp", "hpp"]
     LIBS = get_files(LIBS_DIR, INSTALL_PATH, VALID_SUFFIXES)
     setup(
         name=NAME,
