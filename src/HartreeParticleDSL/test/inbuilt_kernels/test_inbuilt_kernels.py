870c52042f3a4a796c38a9abbfd9a4478eeeb5c7
Aidan Chalk <3043914+LonelyCat124@users.noreply.github.com>
2023-03-29 15:42:27 +0100

    Coverage of boundaries

---
+++ src/HartreeParticleDSL/test/inbuilt_kernels/test_inbuilt_kernels.py
@@ -0,0 +1,5 @@
+from HartreeParticleDSL.inbuilt_kernels.boundaries.boundaries import x_max_sink_boundary, x_min_neutral_source_boundary
+from HartreeParticleDSL.inbuilt_kernels.boundaries.periodic_boundaries import periodic_boundaries
+
+def test_boundaries():
+    pass
